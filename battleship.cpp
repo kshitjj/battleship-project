1dd54aebcbd128f74fc67e9192ce6d45c21e06bd
kshtijj <kshitijrajgude@gmail.com>
2022-08-20 22:56:19 +0530

    made the battleship game

---
+++ battleship.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	// position of the ships on 2x2 board
+	bool gameBoard[5][5] = {
+		{0, 1, 0, 0, 1},
+		{0, 1, 1, 0 ,0},
+		{0, 0, 0, 0 ,0},
+		{1, 0, 0, 0 ,0},
+		{0, 0, 0, 0 ,1}
+	};
+
+	// introduction to the game
+	cout << "Welcome to battleship(CLI edition)" << endl;
+	cout << "To play the game, You need to input 2 coordinates(x,y)" << endl;
+	cout << "These input are the coordinate you attack. So, let's get started" << endl;
+	
+	int hit = 0;
+	int numberOfTurns = 0;
+	//game code
+	while(hit < 6){
+		// taking the x, y coordinate for the attack
+		int x, y;
+		cout << "Enter the x coordinate?(0-4)";
+		cin >> x;
+
+		cout << "Enter the y coordinate?(0-4)";
+		cin >> y;
+		
+		// if the position is true, then it's a hit and hit gets incremented. 
+		// If not number of turns get incremented
+		if (gameBoard[x][y]) {
+			gameBoard[x][y] = false;
+			cout << "Hit!" << endl;
+			hit++;
+			numberOfTurns++;
+		} else {
+			cout << "You missed!" << endl;
+			numberOfTurns++;
+		}	
+	}
+	cout << "Thanks for playing! You won in" << numberOfTurns << "turns" << endl;
+	return 0;
+
+}
